29e3227740ff5747ae756c2554f4527d73c91cac
Zach Sugano <zachoooo@gmail.com>
2012-01-04 14:21:41 -0800

    Lol gotta go

--- src/com/zavteam/plugins/Main.java
+++ src/com/zavteam/plugins/Main.java
@@ -1,11 +1,16 @@
 package com.zavteam.plugins;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	Logger log = Logger.getLogger("Minecraft");
+	protected FileConfiguration config;
+	List<String> messages = new ArrayList();
 	@Override
 	public void onDisable() {
 		saveConfig();
@@ -17,6 +22,11 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		
 		log.info(this + " has been enabled");
+	}
+	public void fullReload() {
+		saveConfig();
+		reloadConfig();
+		messages = config.getStringList("messages");
 	}
 
 }
