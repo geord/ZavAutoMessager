384423558de0402a46d82dfa92feb237a12c6475
Zach Sugano <zachoooo@gmail.com>
2012-01-05 02:08:25 -0800

    Pushing since im goin to bed

--- src/com/zavteam/plugins/Commands.java
+++ src/com/zavteam/plugins/Commands.java
@@ -1,16 +1,24 @@
 package com.zavteam.plugins;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
+	private String noPerm = ChatColor.RED + "You do not have permission to do this";
 	public Main plugin;
 	public Commands(Main instance) {
 		plugin = instance;
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			
+		} else {
+			
+		}
 		return false;
 	}
 
