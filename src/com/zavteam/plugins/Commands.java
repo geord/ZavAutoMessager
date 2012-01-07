c1a1d79aec021bd7b4d015507e1fcb293b450ae1
Zach Sugano <zachoooo@gmail.com>
2012-01-06 18:17:50 -0800

    Working on more command stuff lol

--- src/com/zavteam/plugins/Commands.java
+++ src/com/zavteam/plugins/Commands.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
-	private String noPerm = ChatColor.RED + "You do not have permission to do this";
+	private final static String noPerm = ChatColor.RED + "You do not have permission to do this";
 	public Main plugin;
 	public Commands(Main instance) {
 		plugin = instance;
@@ -15,7 +15,31 @@ public class Commands implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
-			
+			if (args.length == 0) {
+				if (sender.hasPermission("zavautomessager.view")) {
+					sender.sendMessage(ChatColor.GOLD + "===== ZavAutoMessager Help =====");
+					sender.sendMessage(ChatColor.GOLD + "1. /automessager reload - Reloads config");
+					sender.sendMessage(ChatColor.GOLD + "================================");
+				} else {
+					sender.sendMessage(noPerm);
+				}
+			} else if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (sender.hasPermission("zavautomessager.reload")) {
+						plugin.reloadConfig();
+						plugin.messages = plugin.config.getStringList("messages");
+						plugin.delay = plugin.config.getInt("delay", 60);
+						plugin.chatFormat = plugin.config.getString("chatformat", "[&6ZavAutoMessager&f]: %msg");
+						plugin.delay = plugin.delay * 20;
+						plugin.permissionsBV = plugin.config.getBoolean("permissions", false);
+						plugin.saveConfig();
+					} else {
+						sender.sendMessage(noPerm);
+					}
+				} else {
+					
+				}
+			}
 		} else {
 			
 		}
