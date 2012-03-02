29633e3465b0277b88afbae92ee751fe83d0784e
Zach Sugano <zachoooo@gmail.com>
2012-03-01 21:34:53 -0800

    Squashed Console Bug :D

--- src/com/zavteam/plugins/Commands.java
+++ src/com/zavteam/plugins/Commands.java
@@ -4,18 +4,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 	private final static String noPerm = ChatColor.RED + "You do not have permission to do this";
-	//private static int Interval, taskId = -1, counter = 0;
 	public Main plugin;
 	public Commands(Main instance) {
 		plugin = instance;
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (sender instanceof Player) {
 			if (args.length == 0) {
 				if (sender.hasPermission("zavautomessager.view")) {
 					sender.sendMessage(ChatColor.GOLD + "========= ZavAutoMessager Help =========");
@@ -36,6 +33,7 @@ public class Commands implements CommandExecutor {
 						plugin.delay = plugin.delay * 20;
 						plugin.permissionsBV = plugin.config.getBoolean("permissions", false);
 						plugin.saveConfig();
+						sender.sendMessage(ChatColor.GREEN + "ZavAutoMessager's config has been reloaded.");
 					} else {
 						sender.sendMessage(noPerm);
 					}
@@ -44,18 +42,17 @@ public class Commands implements CommandExecutor {
 						plugin.messageToggle = true;
 					} else {
 						sender.sendMessage(noPerm);
-						sender.sendMessage(ChatColor.GREEN + "ZavAutoMessage is now on");
+						sender.sendMessage(ChatColor.GREEN + "ZavAutoMessager is now on");
 					}
 				} else if (args[0].equalsIgnoreCase("off")) {
 					if (sender.hasPermission("zavautomessager.toggle")) {
 						plugin.messageToggle = false;
-						sender.sendMessage(ChatColor.GREEN + "ZavAutoMessage is now off");
+						sender.sendMessage(ChatColor.GREEN + "ZavAutoMessager is now off");
 					} else {
 						sender.sendMessage(noPerm);
 					}
 				}
 			}
-		}
 		return false;
 	}
 }
