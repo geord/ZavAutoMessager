33d33ac2e51450254e7075ef2e4e3c62be0b29d1
Zach Sugano <zachoooo@gmail.com>
2012-03-08 00:05:52 -0800

    RAWR

--- src/com/zavteam/plugins/Configs/MainConfig.java
+++ src/com/zavteam/plugins/Configs/MainConfig.java
@@ -7,6 +7,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import com.zavteam.plugins.Main;
 
 public class MainConfig {
+	// Main Config Handlers
 	public Main plugin;
 	private static FileConfiguration config;
 	public MainConfig(Main instance) {
